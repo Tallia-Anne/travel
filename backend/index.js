06e3c44a3a63ebacd6c4324053f8e559ad05db18
Tallia-Anne <erinawambiekele@gmail.com>
2024-10-08 14:26:48 +0200

    upload

--- backend/index.js
+++ backend/index.js
@@ -219,7 +219,7 @@ app.post('/image-upload', upload.single("image"), async (req, res) => {
             return res.status(400).json({ error: true, message: "No image uploaded" });
         }
 
-        const imageUrl = `https://travel-xh4w.onrender.com/uploads/${req.file.filename}`;
+        const imageUrl = `http://localhost:8000/uploads/${req.file.filename}`;
         res.status(201).json({ imageUrl });
     } catch (error) {
         res.status(500).json({ error: error.message });
